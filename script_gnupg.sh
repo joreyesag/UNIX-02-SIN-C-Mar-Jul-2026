eda3381183680fc7b1529f059f1bdbf5f3c0fac5
Jordan Reyes <joreyesag@uide.edu.ec>
2026-03-26 14:01:02 -0500

    feat: complete Block D using Jordan's identity (-u joreyesag)

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 gpg --decrypt doc_cifrado.txt
+gpg -u joreyesag@uide.edu.ec --clearsign --output documento_jordan_clearsign.asc documento_uide.txt
+gpg -u joreyesag@uide.edu.ec --armor --detach-sign --output documento_jordan_detached.sig documento_uide.txt
