2f513aeaece34a1b39cd4b84db361a253b4a738f
Jordan Reyes <joreyesag@uide.edu.ec>
2026-03-26 14:05:09 -0500

    chore: finalize repository with full Web of Trust validation

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -2,3 +2,6 @@
 gpg --decrypt doc_cifrado.txt
 gpg -u joreyesag@uide.edu.ec --clearsign --output documento_jordan_clearsign.asc documento_uide.txt
 gpg -u joreyesag@uide.edu.ec --armor --detach-sign --output documento_jordan_detached.sig documento_uide.txt
+# FINAL VALIDATION: Web of Trust verified
+gpg --list-sigs macharroja@uide.edu.ec
+gpg --verify doc_uide_dual_final.gpg
