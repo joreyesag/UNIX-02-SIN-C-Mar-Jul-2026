e4d1dc0d9ec992601b0c981c03c7c70ccf2d872f
Jordan Reyes <joreyesag@uide.edu.ec>
2026-03-26 14:06:33 -0500

    feat: complete Block F - simultaneous encrypt and sign

--- script_gnupg.sh
+++ script_gnupg.sh
@@ -5,3 +5,6 @@ gpg -u joreyesag@uide.edu.ec --armor --detach-sign --output documento_jordan_det
 # FINAL VALIDATION: Web of Trust verified
 gpg --list-sigs macharroja@uide.edu.ec
 gpg --verify doc_uide_dual_final.gpg
+# BLOCK F: Simultaneous Encryption and Signing
+gpg -u joreyesag@uide.edu.ec -r macharroja@uide.edu.ec --encrypt --sign --armor --output mensaje_final_seguro.asc documento_uide.txt
+gpg --decrypt mensaje_final_seguro.asc
