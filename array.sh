664a72a4175a7b56655e44e77b9d7330d59c8ec1
joreyesag <joreyesag@uide.edu.ec>
2026-06-05 14:41:21 +0000

    create the array.sh script

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#sets an array
+IP_ADRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
+
+#Print all the elements in the array 
+echo "${IP_ADDRESSES[*]}"
+
+#prints only the first element in the array
+echo "${IP_ADDRESSES[*]}"
+
