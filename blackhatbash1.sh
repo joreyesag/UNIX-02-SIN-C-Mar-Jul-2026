79216e5fbc14f6bdc00c1c4524815a6a4a7ceddb
joreyesag <joreyesag@uide.edu.ec>
2026-06-05 09:17:29 -0500

    Create blackhatbash1.sh

---
+++ blackhatbash1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#!/usr/bin/env bash
+set -x
+
+bash --version
+
+env
+
+echo ${SHELL}
+
+echo ${RANDOM}
+
+echo ${UID}
+
+echo ${OSTYPE}
+
+ps -e -f
+
+df --human-readable
+
+# bash -x blackhatbash1.sh
+
+# bash -r blackhatbash1.sh
+
+# chmod u+x blackhatbash1.sh
+
+# ./blackhatbash1.sh
+
+# bash -n blackhatbash1.sh
+
+# bash blackhatbash1.sh
+
+set +x
