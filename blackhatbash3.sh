33472caedd40c7a7757560c982d320d9a21b189e
joreyesag <joreyesag@uide.edu.ec>
2026-06-05 09:16:36 -0500

    Add blackhatbash3.sh script with book info

---
+++ blackhatbash3.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+book="black hat bash"
+echo "This book's name is ${book}"
+echo "This book's name is $book"
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+unset book
+echo "${book}"
