480711b1bab7768e1421734ace335cfd8d1138f7
joreyesag <joreyesag@uide.edu.ec>
2026-06-05 09:16:59 -0500

    Create blackhatbash2.sh

---
+++ blackhatbash2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# All this script does is create a directory, create a file
+
+# within the directory, and then list the contents of the directory.
+
+mkdir mydirectory
+
+set -x
+
+touch mydirectory/myfile
+ls -l mydirectory
+
+set +x
